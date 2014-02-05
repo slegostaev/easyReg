65a8181c2b57ab339101a1bd63a2a47db82a2bb3
Legostaev Sergey <sergey.legostaev@siliconrussia.com>
2014-02-05 14:34:01 +0400

    updated sql scripts

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -33,17 +33,3 @@ insert into chairs (id, location, unit_id) values (4, 'Kab 4', 1);
 insert into chairs (id, location, unit_id) values (5, 'Kab 3 right', 1);
 
 
-/*insert into receptions (id, patient_id, doctor_id, reception_date, start_time, end_time, description)
-	values (nextval('patients_seq'), 1, 1, '2014-01-28', '09:00:00', '10:00:00', 'first patient');
-
-insert into receptions (id, patient_id, doctor_id, reception_date, start_time, end_time, description)
-	values (nextval('patients_seq'), 2, 1, '2014-01-28', '10:00:00', '11:00:00', '2nd patient');
-	
-insert into receptions (id, patient_id, doctor_id, reception_date, start_time, end_time, description)
-	values (nextval('patients_seq'), 3, 2, '2014-01-29', '09:00:00', '10:00:00', '3 patient');
-	
-insert into receptions (id, patient_id, doctor_id, reception_date, start_time, end_time, description)
-	values (nextval('patients_seq'), 4, 3, '2014-01-28', '09:00:00', '10:00:00', '4 patient');
-	
-insert into receptions (id, patient_id, doctor_id, reception_date, start_time, end_time, description)
-	values (nextval('patients_seq'), 5, 3, '2014-02-02', '12:00:00', '13:00:00', '5 patient');*/
--- conf/evolutions/default/test.sql
+++ conf/evolutions/default/test.sql
@@ -1,1 +1,14 @@
-select * from users;
+/*insert into receptions (id, patient_id, doctor_id, reception_date, start_time, end_time, description)
+	values (nextval('patients_seq'), 1, 1, '2014-01-28', '09:00:00', '10:00:00', 'first patient');
+
+insert into receptions (id, patient_id, doctor_id, reception_date, start_time, end_time, description)
+	values (nextval('patients_seq'), 2, 1, '2014-01-28', '10:00:00', '11:00:00', '2nd patient');
+	
+insert into receptions (id, patient_id, doctor_id, reception_date, start_time, end_time, description)
+	values (nextval('patients_seq'), 3, 2, '2014-01-29', '09:00:00', '10:00:00', '3 patient');
+	
+insert into receptions (id, patient_id, doctor_id, reception_date, start_time, end_time, description)
+	values (nextval('patients_seq'), 4, 3, '2014-01-28', '09:00:00', '10:00:00', '4 patient');
+	
+insert into receptions (id, patient_id, doctor_id, reception_date, start_time, end_time, description)
+	values (nextval('patients_seq'), 5, 3, '2014-02-02', '12:00:00', '13:00:00', '5 patient');*/
